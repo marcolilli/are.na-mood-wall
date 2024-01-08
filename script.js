6a55dff26227745412044b59f74db580695a33b4
marcolilli <mail@marcolilli.com>
2024-01-08 19:20:34 +0100

    Add fullscreen mode

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ const urlParams = new URLSearchParams(window.location.search);
 
 const channelId = urlParams.get("channelId") || "five-ixlsr8b94mc";
 const fpsString = urlParams.get("fps") || "1";
+const fullScreen = urlParams.get("fullscreen") === "true" || false;
 const orderBy = urlParams.get("orderBy") || null;
 
 let images = []; // array to store the images
 let currentImageIndex = 0; // variable to keep track of the current image
+
+let requestsNeeded = 0;
+let requestsCounter = 0;
 
 function preload() {
   const pageSize = 100;
@@ -19,12 +23,12 @@ function preload() {
       }
       blocksInChannel = data.length;
 
-      const requestsNeeded = Math.ceil(blocksInChannel / pageSize);
+      requestsNeeded = Math.ceil(blocksInChannel / pageSize);
 
-      for (let i = 0; i < requestsNeeded; i++) {
+      const fetchImages = () => {
         fetch(
           `https://api.are.na/v2/channels/${channelId}/contents?per=${pageSize}&page=${
-            i + 1
+            requestsCounter + 1
           }`
         )
           .then((response) => response.json())
@@ -46,8 +50,17 @@ function preload() {
           })
           .catch((error) => {
             console.error(error);
+          })
+          .finally(() => {
+            requestsCounter++;
+
+            if (requestsCounter < requestsNeeded) {
+              fetchImages();
+            }
           });
-      }
+      };
+
+      fetchImages();
     });
 }
 
@@ -56,23 +69,45 @@ function setup() {
   frameRate(parseInt(fpsString));
 
   background(25);
-  textAlign(CENTER, CENTER);
-  fill(100);
-  textSize(20);
-  text("Loading…", width / 2, height / 2);
 }
 
 function draw() {
-  imageMode(CENTER);
-
-  if (!mouseIsPressed) {
+  if (requestsCounter < requestsNeeded) {
+    background(25);
+    textAlign(CENTER, CENTER);
+    fill(100);
+    textSize(20);
+    text(
+      `Loading (${requestsCounter}/${requestsNeeded})…`,
+      width / 2,
+      height / 2
+    );
+  } else if (!mouseIsPressed) {
     const currentImage = images[currentImageIndex];
 
     if (currentImage) {
+      let drawWidth;
+      let drawHeight;
+
+      if (!fullScreen) {
+        drawWidth = currentImage.width;
+        drawHeight = currentImage.height;
+      } else {
+        if (windowWidth > windowHeight) {
+          drawWidth = windowWidth;
+          drawHeight = (windowWidth / currentImage.width) * currentImage.height;
+        } else {
+          drawWidth = (windowHeight / currentImage.height) * currentImage.width;
+          drawHeight = windowHeight;
+        }
+      }
+
       drawingContext.drawImage(
         currentImage,
-        width / 2 - currentImage.width / 2,
-        height / 2 - currentImage.height / 2
+        width / 2 - drawWidth / 2,
+        height / 2 - drawHeight / 2,
+        drawWidth,
+        drawHeight
       );
 
       currentImageIndex = (currentImageIndex + 1) % images.length;
